c350638c359e8a0da6ef438b573c6a9da0d05e9e
lexicalunit <lexicalunit@lexicalunit.com>
2015-03-20 19:05:33 -0500

    Better error handle.

--- upload.sh
+++ upload.sh
@@ -2,16 +2,16 @@
 
 # Uploads directory tree at "new" to remote music NAS, then locally syncs.
 
-set -uex
+set -ux
 
 # delete cruft
 find . -name .DS_Store -print0 | xargs -0 rm -f
 
-# upload new songs to NAS
-rsync -av --progress new/* /Volumes/NAS/music/.
-
 # sync new songs to local mirror
 rsync -av --progress new/* .
+
+# upload new songs to NAS
+rsync -av --progress new/* /Volumes/NAS/music/.
 
 # clean out new songs (they're in the NAS and local mirror now)
 find new -type f -print0 | xargs -0 rm -f
